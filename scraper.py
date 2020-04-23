e4d501d4f01b727fc2c17c71769326abda41edb2
Anthony Esmeralda <aresmeralda@gmail.com>
2020-04-23 00:11:50 -0700

    literally pulled my hair out

--- scraper.py
+++ scraper.py
@@ -5,27 +5,45 @@ import urllib.robotparser
 
 
 visited = set()
+stopwords = []
 
-def least_split_length(url1, url2):
-    ''' Returns the smallest length between two split urls'''
-    return min(len(url1), len(url2))
+def in_web_trap(url):
+    parsed = urlparse(url)
+    path_list = parsed.path[1:].split('/')
+    if len(path_list) != len(set(path_list)):
+        return True
+    else:
+        return False
 
-
+def is_html_text(resp):
+    try:
+        if re.search("text|html", resp.raw_response.headers['content-type']) is not None:
+            return True
+        else:
+            return False
+    except:
+        return False
+        
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.                                  
+    # Implementation requred.                        
     links = []
-    if resp.status in range(200,300):                       # we're getting a successful response from the url.
-        visited.add(url)                                    # add the url into our visited list
-        content = resp.raw_response.content                 # grab the html content from the url
-        soup = BeautifulSoup(content, "html.parser")        # soup the content
-        for a in soup.find_all('a', href=True):             # find all links
-            potential_url = define_url(url,a['href'])
-            if is_valid(potential_url) and check_valid_domain(potential_url) and potential_url not in visited:
+    if url in visited or in_web_trap(url) and not is_html_text(resp):
+        return links
+    visited.add(url)
+    if resp.status in range(200,300):                               # we're getting a successful response from the url.
+        content = resp.raw_response.content                         # grab the html content from the url
+        soup = BeautifulSoup(content, "html.parser")                # soup the content
+        if low_information_page(soup):
+            return links
+        for a in soup.find_all('a', href=True):                     # find all links
+            potential_url = urllib.parse.urljoin(url,a['href'])
+            if is_valid(potential_url) and check_valid_domain(potential_url) and potential_url not in visited and not in_web_trap(potential_url):
                 links.append(potential_url)
+        prev_resp = content
     return links
 
 def is_valid(url):
@@ -36,57 +54,63 @@ def is_valid(url):
 
         #encountered a case "http://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018" which was a pdf, but still output True
         # so i modified this code a little bit. to where if it was a .extension or the extension existed in the path
-        extension_types = "(css|js|bmp|gif|jpe?g|ico|" \
+        # also added: war and apk ...
+        extension_types = "(css|js|bmp|gif|jpe?g|ico|img|" \
                           "png|tiff?|mid|mp2|mp3|mp4|" \
                           "wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|" \
                           "ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|" \
                           "data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|" \
-                          "epub|dll|cnf|tgz|sha|" \
+                          "epub|dll|cnf|tgz|sha|apk" \
                           "thmx|mso|arff|rtf|jar|csv|"\
-                          "rm|smil|wmv|swf|wma|zip|rar|gz)"
+                          "rm|smil|wmv|swf|wma|zip|rar|gz|war)"
 
         if re.search("(/)" + extension_types, parsed.path.lower()):
             return False
         if re.search("(.)" + extension_types, parsed.path.lower()):
             return False
-        
+        """
+            # - is a scroll location
+            event - can lead to a calendar
+            gallery - can lead to a photo album
+            upload - can lead to something with a file that might be missed in extension_type
+            index.php - can lead us to getting stuck in some sort of weird trap
+        """
+        if '#' in url or "event" in url or "gallery" in url or "upload" in url or "index.php" in url:
+            return False
+        # honestly, it would take forever if we handled every query which can be in the thousands. (mostly news on the given domains)
+        if len(parsed.query) != 0:
+            return False
         return True
 
     except TypeError:
         print ("TypeError for ", parsed)
         raise
 
-"""
-    base -> the url given from the frontier
-    path -> url(s) from the frontier's html code
-
-    (1) if ... it has no scheme and it has no netloc, meaning that it must be in form --- /xyz --- (meaning that it is a path from the base url)
-    (2) elif ... it has no scheme but is in form --- //*.asdfasdf.* (meaning that it might be another website but formated wrong)
-    (3) it takes on the form of http://www.somewebsite.com/random... so just return it as is.
-"""
-def define_url(base, path):
-    base_parsed = urlparse(base)
-    path_parsed = urlparse(path)
-    if path_parsed.scheme == "" and path_parsed.netloc == "":
-        new_base = base_parsed.scheme + "://" + base_parsed.netloc
-        return new_base + path
-    elif path_parsed.scheme == "" and path_parsed.netloc is not "":
-        return base_parsed.scheme + ":" + path
-    else:
-        return path
-
-def ok_status(url):
-    r = requests.get(url)
-    if r.status_code in range(200,300):
-        return True
-    else:
-        return False
-
 def check_valid_domain(url):
-    accepted_domains = "(\.ics\.uci\.edu|\.cs\.uci\.edu|informatics\.uci\.edu|\.stat\.uci\.edu|today\.uci\.edu\/department\/information_computer_sciences)"
+    # accept domains only from *.ics.uci.edu/ , *.cs.uci.edu , *.informatics.uci.edu , *.stat.uci.edu and the last one
+    accepted_domains = "(\.ics\.uci\.edu|\.cs\.uci\.edu|\.informatics\.uci\.edu|\.stat\.uci\.edu|today\.uci\.edu\/department\/information_computer_sciences)"
     if not re.search(accepted_domains,urlparse(url).netloc):
         return False
     return True
+
+def low_information_page(soup):
+    global stopwords
+    token_list = []
+    if stopwords == []:
+        with open("stopwords.txt", 'r') as fd:
+            for line in fd:
+                stopwords.append(line.replace("\n",""))
+    try:
+        content = "".join([p.text for p in soup.find_all("p")])                 # get all the text within a p tag (meat of the website)
+        content = content.split()                                               # split it into a list
+        for token in content:                                                   
+            if token not in stopwords:                                          # if the token is not a stop word then...
+                token_list.append(token)
+        if len(content) < 150 or (len(token_list)/len(content) < 0.25):         # if we dont have alot of words then probably dont use it.
+            return True
+        return False
+    except:
+        return True
 
 def robot_checker(url):                                         # robot checker (refer to docs.python.org/3/library/urllib.robotparser.html)
     parsed_url = urlparse(url)
